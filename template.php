541cf7bbe3cb496e6c7dfc397d11470f4aebeb77
Aegir <butsko.7@osu.edu>
2012-02-15 16:29:39 -0500

    Initial commit.

---
+++ template.php
@@ -0,0 +1,10 @@
+<?php
+
+// The QuickSites layout (can be overridden by dependant themes)
+$GLOBALS['quickSites_layout'] = 'b';
+
+// These content types will not display a title in inner.tpl.php
+$GLOBALS['no_title_node_types'] = array("qs_events", "qs_people", "event");
+
+
+?>
