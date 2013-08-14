497c774daf53ce517bfe6ffc9ae673b0826d6178
Dustin Perzanowski <perzanowski.4@osu.edu>
2013-08-14 14:11:33 -0400

    check to see if page is served from HTTPs and provide proper URL to OSU webfonts

--- template.php
+++ template.php
@@ -7,10 +7,16 @@ $GLOBALS['quickSites_layout'] = 'b';
 $GLOBALS['no_title_node_types'] = array("qs_events", "qs_people", "event");
 
 function asc_qs_preprocess_page(&$vars, $hook) {
+	if (empty($_SERVER['HTTPS'])) {
+		$osu_webfont_url = 'http://www.osu.edu/assets/fonts/webfonts.css';
+	}
+	else {
+		$osu_webfont_url = 'https://www.osu.edu/assets/fonts/webfonts.css';
+	}
   $vars['head'] .= '<link '. drupal_attributes(array(
     'rel' => 'stylesheet',
     'type' => 'text/css',
-    'href' => 'http://www.osu.edu/assets/fonts/webfonts.css')
+    'href' => $osu_webfont_url)
   ) ." />\n";
 }
 
